c4ba9374998f6083d2833ce45975ee72dadef7b9
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2022-12-01 07:08:49 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let  totalBatteries = 0;
+// use. reduce method to find total number of batteries 
+
+totalBatteries = batteryBatches.reduce((total, batch) =>{
+    return total + batch;
+} , 0);
+
+console.log(totalBatteries);
+
